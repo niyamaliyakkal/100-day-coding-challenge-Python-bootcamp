0a846207e7602fe00d1e7f3efc2edcd900db1587
niyamaliyakkal <162799864+niyamaliyakkal@users.noreply.github.com>
2024-06-15 16:27:18 +0530

    Add files via upload
    Let's play rock, paper, scissor game.

--- main.py
+++ main.py
@@ -1,7 +1,56 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-print("Welcome to the tip calculator!")
-bill=float(input("What was the total bill?$"))
-tip=float(input("How much tip would you like to give?\n10, 12, or 15?"))
-split=float(input("How many people to split the bill?"))
-total= (bill+(bill*(tip/100)))/split
-print(f"Each person should pay:${(round(total, 2)) }")
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+user=input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+print("You chose:")
+if user=="0":
+    print(rock)
+elif user=="1":
+    print(paper)
+elif user=="2":
+    print(scissors)
+else:
+    print("Invaid input!")
+mylist=["rock","paper","scissors"]
+import random
+n=random.choice(mylist)
+print("Computer chose:")
+if n=="rock":
+    print(rock)
+elif n=="paper":
+    print(paper)
+elif n=="scissors":
+    print(scissors)
+if user=="0" and n=="scissors":
+    print("You win!")
+elif user=="2" and n=="paper":
+    print("You win!")
+elif user=="1" and n=="rock":
+    print("You win!")
+else:
+    print("You lose! Try again :) ")
