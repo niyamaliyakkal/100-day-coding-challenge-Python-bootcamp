1de02a5809b4edd2cb0677349ae01bbb09ad2cec
niyamaliyakkal <162799864+niyamaliyakkal@users.noreply.github.com>
2024-06-15 16:33:20 +0530

    https://replit.com/@niyamaliyakkal/treasure-island-start-2#main.py
    Let's play another game. Treasure island.

---
+++ Treasure island.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+x=input("You\'re at a cross road. Where do you want to go?\nType \"left\" or \"right\"\n")
+if x=="left":
+    print("You\'ve come to a lake. There is an island in the middle of the lake.\n   Type \"wait\" to wait for a boat. Type \"swim\" to swim across. ")
+    y=input()
+    if y=="wait":
+        print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")   
+        z=input()      
+        if z=="red":
+            print("It is a room full of fire. Game Over!")
+        elif z=="yellow":
+            print("Congratulations. You found the treasure! You Win!")
+        else:
+            print("You enter a wrong room. Game Over!")      
+    else:
+        print("There is a crocodile in the lake. Sorry you died. Game Over!")
+else:
+    print("Wrong route kid. Game Over!")      
+                          
+
